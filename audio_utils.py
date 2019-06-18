76ac8e2e8cab3451734ec3bd656fcf8e1e183c8a
Max Elliott <maxelliott456@outlook.com>
2019-06-18 14:05:05 +0100

    Adding data processing scripts.

---
+++ audio_utils.py
@@ -0,0 +1,136 @@
+from IPython.display import Audio
+from scipy.io import wavfile
+import os
+import librosa
+import librosa.display
+import numpy as np
+import torch
+import torch.nn as nn
+import copy
+import matplotlib.pyplot as plt
+
+
+# dataset_dir = "/Users/Max/MScProject/datasets/IEMOCAP"
+# dataset_dir = "/Users/Max/MScProject/test_dir"
+
+class hyperparams(object):
+    def __init__(self):
+        self.sr = 16000 # Sampling rate. Paper => 24000
+        self.n_fft = 1024 # fft points (samples)
+        self.frame_shift = 0.0125 # seconds
+        self.frame_length = 0.05 # seconds
+        self.hop_length = int(self.sr*self.frame_shift) # samples  This is dependent on the frame_shift.
+        self.win_length = int(self.sr*self.frame_length) # samples This is dependent on the frame_length.
+        self.n_mels = 80 # Number of Mel banks to generate
+        self.power = 1.2 # Exponent for amplifying the predicted magnitude
+        self.n_iter = 100 # Number of inversion iterations
+        self.use_log_magnitude = True # if False, use magnitude
+        self.preemph = 0.97
+
+hp = hyperparams()
+
+def load_wav(path):
+    return wavfile.read(path)[1]
+
+def save_wav(wav, path):
+    wav *= 32767 / max(0.01, np.max(np.abs(wav)))
+    wavfile.write(path, hp.sr, wav.astype(np.int16))
+
+def wav2spectrogram(y, sr = hp.sr):
+
+    '''
+    Produces log-magnitude spectrogram of audio data y
+    '''
+
+    spec = librosa.core.stft(y, n_fft = hp.n_fft, hop_length = hp.hop_length,
+                                                win_length = hp.win_length)
+    spec_mag = amp_to_db(np.abs(spec))
+    # spec_angle = np.angle(spec)
+    # spec_mag = lowpass(spec_mag, 400)
+
+    return spec_mag
+
+def lowpass(spec, frequency):
+    return spec[:frequency, :]
+
+def wav2melspectrogram(y, sr = hp.sr, n_mels = hp.n_mels):
+    '''
+    y = input wav file
+    '''
+    mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels = n_mels,
+        n_fft = hp.n_fft, hop_length = hp.hop_length, power = hp.power)
+
+    return mel_spec
+
+def spectrogram2melspectrogram(spec, n_fft = hp.n_fft, n_mels = hp.n_mels):
+
+    if isinstance(spec, torch.Tensor):
+        spec = spec.numpy()
+
+    mels = librosa.filters.mel(hp.sr, n_fft, n_mels = n_mels)
+    return mels.dot(spec**hp.power)
+
+def melspectrogram2wav(mel):
+    '''
+    Needs doing: impossible?
+    '''
+    return 0
+
+def spectrogram2wav(spectrogram):
+    '''
+    spectrogram: [t, f], i.e. [t, nfft // 2 + 1]
+    '''
+    if isinstance(spectrogram, torch.Tensor):
+        spectrogram = spectrogram.numpy()
+
+    spectrogram = db_to_amp(spectrogram)#**hp.power
+
+    X_best = copy.deepcopy(spectrogram)  # [f, t]
+    for i in range(hp.n_iter):
+
+        X_t = invert_spectrogram(X_best)
+        # print(X_t.shape())
+        est = librosa.stft(X_t, hp.n_fft, hp.hop_length, win_length=hp.win_length)  # [f, t]
+        phase = est / np.maximum(1e-8, np.abs(est))  # [f, t]
+        X_best = spectrogram * phase  # [f, t]
+    X_t = invert_spectrogram(X_best)
+
+    return np.real(X_t)
+
+def invert_spectrogram(spectrogram):
+    '''
+    spectrogram: [f, t]
+    '''
+    return librosa.istft(spectrogram, hp.hop_length, win_length=hp.win_length, window="hann")
+
+def wav2mcep(y, sr = hp.sr, n_mfcc = 30):
+    '''
+    y = input wav file
+    '''
+    mfccs = librosa.feature.mfcc(y=ym, sr=sr, n_mfcc=n_mfcc)
+
+    return mfccs
+
+def mcep2wav(mfccs):
+    '''
+    Needs doing
+    '''
+    return 0
+
+def amp_to_db(spec):
+    return librosa.core.amplitude_to_db(spec)
+
+def db_to_amp(spec):
+    return librosa.core.db_to_amplitude(spec)
+
+def plot_spec(spec, type = 'mel'):
+
+    plt.figure(figsize=(6, 4))
+
+    if isinstance(spec, torch.Tensor):
+        spec = spec .numpy()
+    librosa.display.specshow(spec, y_axis=type, sr=hp.sr, hop_length=hp.hop_length,
+                                                    fmin=None, fmax=4000)
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Power spectrogram')
+    plt.show()
---
+++ data_preprocessing.py
@@ -0,0 +1,241 @@
+from IPython.display import Audio
+from scipy.io import wavfile
+import os
+import librosa
+import numpy as np
+import torch
+import torch.nn as nn
+import audio_utils
+import pickle
+
+dataset_dir = "/Users/Max/MScProject/datasets/IEMOCAP"
+n_speakers = 10
+n_emotions = 7
+# dataset_dir = "/Users/Max/MScProject/datasets/test_dir"
+
+# def speaker2onehot(filename):
+#
+#     onehot = np.zeros(n_speakers)
+#     code = filename[4] + filename[-8]
+#
+#     conversion = {'1F':0, '1M':1, '2F':2, '2M':3, '3F':4, '3M':5, '4F':6, '4M':7,
+#                     '5F': 8, '5M':9}
+#
+#     onehot[conversion[code]] = 1
+#
+#     return onehot
+#
+# def category2onehot(category):
+#
+#     onehot = np.zeros(7, dtype = np.float32)
+#
+#     if category == 'xxx' or category =='dis' or category =='fea' or category == 'oth':
+#         return onehot
+#
+#     conversion = {'hap':0, 'sad':1, 'ang':2, 'exc':3, 'fru':4, 'sur':5, 'neu':6}
+#
+#     onehot[conversion[category]] = 1
+#
+#     return onehot
+
+def get_speaker_from_filename(filename):
+    code = filename[4] + filename[-8]
+
+    conversion = {'1F':0, '1M':1, '2F':2, '2M':3, '3F':4, '3M':5, '4F':6, '4M':7,
+                    '5F': 8, '5M':9}
+
+    label = conversion[code]
+
+    return label
+
+def get_emotion_from_label(category):
+
+    if category == 'xxx' or category =='dis' or category =='fea' or category == 'oth':
+        return -1
+    if category == 'exc' or category == 'fru' or category == 'sur':
+        return -1
+
+    conversion = {'hap':0, 'sad':1, 'ang':2, 'neu':3, 'exc':4, 'fru':5, 'sur':6}
+
+    label = conversion[category]
+
+    return label
+
+def getOneHot(label, n_labels):
+
+    onehot = np.zeros(n_labels)
+    onehot[label] = 1
+
+    return onehot
+
+def cont2list(cont, binned = False):
+
+    list = np.zeros(3)
+    list[0] = float(cont[1:6])
+    list[1] = float(cont[9:14])
+    list[2] = float(cont[17:22])
+
+    #Option to make the values discrete: low(0), med(1) or high(2)
+    if binned:
+        for i, val in enumerate(list):
+            if val < 2:
+                list[i] = 0
+            elif val < 4:
+                list[i] = 1
+            else:
+                list[i] = 2
+        return list
+    else:
+        return list
+
+def get_wav_and_labels(filename, session_dir):
+
+    folder = filename[:-9]
+    wav_path = session_dir + "/" + folder + "/" + filename
+    label_path = session_dir + "/Annotations/" + folder + ".txt"
+
+    with open(label_path, 'r') as label_file:
+
+        category = ""
+        dimensions = ""
+        speaker = ""
+
+        for row in label_file:
+            if row[0] == '[':
+                split = row.split("\t")
+                if split[1] == filename[:-4]:
+                    category = get_emotion_from_label(split[2])
+                    dimensions = cont2list(split[3])
+                    dimensions_dis = cont2list(split[3], binned = True)
+                    speaker = get_speaker_from_filename(filename)
+
+
+    audio = audio_utils.load_wav(wav_path)
+    audio = np.array(audio, dtype = np.float32)
+
+    return audio, category, dimensions, dimensions_dis, speaker
+
+def get_session_data(data_dir, exclude_unlabelled = True):
+
+    # print(data_dir)
+    os.chdir(data_dir)
+
+    filenames = []
+    specs = []
+    mels = []
+    labels = []
+    conts = []
+    conts_dis = []
+    speakers = []
+    for foldername in os.listdir(data_dir):
+
+        if not (foldername == "Annotations" or foldername == ".DS_Store"):
+
+            for filename in os.listdir(data_dir + "/" + foldername):
+
+                if not filename == ".DS_Store":
+                    wav, label, cont, cont_dis, speaker = get_wav_and_labels(filename,
+                                                                    data_dir)
+
+                    if not (exclude_unlabelled and label == -1): #ignore some rare emotions
+                        mel = audio_utils.wav2melspectrogram(wav)
+                        spec = audio_utils.wav2spectrogram(wav)
+
+                        filenames.append(filename)
+                        mels.append(torch.Tensor(mel))
+                        specs.append(torch.Tensor(spec))
+
+                        labels.append(label)
+                        conts.append(cont)
+                        conts_dis.append(cont_dis)
+                        speakers.append(speaker)
+
+        print(foldername + " completed.")
+
+    return filenames, mels, specs, labels, conts, conts_dis,  speakers
+
+def all_wavs_and_labels(exclude_unlabelled = True):
+
+    os.chdir(dataset_dir)
+
+    filenames = []
+    specs = []
+    mels = []
+    labels = []
+    conts = []
+    conts_dis = []
+    speakers = []
+
+    for foldername in os.listdir(dataset_dir):
+
+        if not (foldername == "Annotations" or foldername == ".DS_Store" or foldername == "Processed"):
+
+            for filename in os.listdir(dataset_dir + "/" + foldername):
+
+                if not filename == ".DS_Store":
+                    wav, label, cont, cont_dis, speaker = audio_utils.get_wav_and_labels(filename)
+
+                    if not (exclude_unlabelled and label == -1): #ignore some rare emotions
+
+                        spec = audio_utils.wav2spectrogram(wav)
+                        mel = audio_utils.spectrogram2melspectrogram(spec)
+
+                        filenames.append(filename)
+                        mels.append(torch.Tensor(mel))
+                        specs.append(torch.Tensor(spec))
+
+                        labels.append(label)
+                        conts.append(cont)
+                        conts_dis.append(cont_dis)
+                        speakers.append(speaker)
+        print(foldername + " done.")
+
+    return filenames, mels, specs, labels, conts, conts_dis, speakers
+
+def save_data(data, filename):
+    with open(filename, 'wb') as fp:
+        pickle.dump(data, fp)
+
+def load_data(filename):
+    with open(filename, 'rb') as fp:
+
+        data = pickle.load(fp)
+
+    return data
+
+if __name__ == "__main__":
+
+    for i in range(1,6):
+
+        ses_number = str(i)
+
+        session_dir = dataset_dir + "/Session" + ses_number
+
+        filenames, mels, specs, labels, conts, conts_dis, speakers = get_session_data(
+                                            session_dir, exclude_unlabelled = True)
+
+        # print(len(mels))
+        save_data(filenames, dataset_dir + '/Processed_data/filenames' + ses_number)
+        save_data(mels, dataset_dir + '/Processed_data/melspecs' + ses_number)
+        save_data(specs, dataset_dir + '/Processed_data/specs' + ses_number)
+        save_data(labels, dataset_dir + '/Processed_data/labels' + ses_number)
+        save_data(conts, dataset_dir + '/Processed_data/conts' + ses_number)
+        save_data(conts_dis, dataset_dir + '/Processed_data/conts_dis' + ses_number)
+        save_data(speakers, dataset_dir + '/Processed_data/speakers' + ses_number)
+        print('Done ' + ses_number + ".")
+
+    # wav = get_wav_and_labels("Ses01F_impro03_F001.wav", dataset_dir + "/Session1")[0]
+    # wav2 = get_wav_and_labels("Ses01F_impro03_F002.wav", dataset_dir + "/Session1")[0]
+    #
+    # spec = audio_utils.wav2spectrogram(wav)
+    # spec2 = audio_utils.wav2spectrogram(wav2)
+    # print(spec.shape)
+    # print(spec2.shape)
+    # audio_utils.plot_spec(spec, type = 'log')
+    #
+    # melspec = audio_utils.spectrogram2melspectrogram(spec)
+    # audio_utils.plot_spec(melspec)
+    #
+    # reproduced = audio_utils.spectrogram2wav(spec)
+    #
+    # audio_utils.save_wav(reproduced, "/Users/Max/MScProject/datasets/Produced/test1.wav")
